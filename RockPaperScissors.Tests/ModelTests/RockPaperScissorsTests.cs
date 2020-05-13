cbcf18265a6afe92aeafb8b5588ff3ae721ec347
Matt Stroud <lixingke06@gmail.com>
2020-05-13 11:10:48 -0700

    add test for getting random computer choice

--- RockPaperScissors.Tests/ModelTests/RockPaperScissorsTests.cs
+++ RockPaperScissors.Tests/ModelTests/RockPaperScissorsTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using Game.Models;
 
 namespace Game.Tests
@@ -15,6 +16,18 @@ namespace Game.Tests
       string result = round.UserChoice;
 
       Assert.AreEqual(result, userChoice);
+    }
+
+    [TestMethod]
+    public void Constructor_ChoosesRandomChoiceForComputer_ComputerChoice()
+    {
+      List<string> computerChoices = new List<string> { "paper", "rock", "scissors" };
+      RockPaperScissors round = new RockPaperScissors("test");
+
+      string computerChoice = round.ComputerChoice;
+      bool result = computerChoices.Contains(computerChoice);
+
+      Assert.AreEqual(true, result);
     }
   }
 }
