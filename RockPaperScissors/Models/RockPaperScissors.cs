86dc4866d7ade2e04e4c380cb0b4b493c976f5e3
Matt Stroud <lixingke06@gmail.com>
2020-05-13 11:24:02 -0700

    add win list and code to GetGameResult to determine win conditions

--- RockPaperScissors/Models/RockPaperScissors.cs
+++ RockPaperScissors/Models/RockPaperScissors.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Game.Models
 {
@@ -7,6 +8,12 @@ namespace Game.Models
     public string UserChoice { get; set; }
     public string ComputerChoice { get; set; }
     public static string[] _computerChoices = { "rock", "paper", "scissors" };
+    public static Dictionary<string, string> winList = new Dictionary<string, string>
+    {
+      { "rock", "scissors" },
+      { "scissors", "paper" },
+      { "paper", "rock" }
+    };
 
     public RockPaperScissors(string userChoice)
     {
@@ -18,9 +25,14 @@ namespace Game.Models
     public string GetGameResult()
     {
       string result = "";
+
       if (UserChoice == ComputerChoice)
       {
         result = "draw";
+      }
+      else if (winList.ContainsKey(UserChoice) && winList[UserChoice] == ComputerChoice)
+      {
+        result = "win";
       }
 
       return result;
