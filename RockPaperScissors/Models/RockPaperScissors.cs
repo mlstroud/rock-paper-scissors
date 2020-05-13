8ebea3352bd31c4e360463af8144a7355fa06396
Matt Stroud <lixingke06@gmail.com>
2020-05-13 11:13:38 -0700

    add code to constructor to generate random computer choice

--- RockPaperScissors/Models/RockPaperScissors.cs
+++ RockPaperScissors/Models/RockPaperScissors.cs
@@ -1,13 +1,18 @@
+using System;
+
 namespace Game.Models
 {
   public class RockPaperScissors
   {
     public string UserChoice { get; set; }
     public string ComputerChoice { get; set; }
+    public static string[] _computerChoices = { "rock", "paper", "scissors" };
 
     public RockPaperScissors(string userChoice)
     {
       UserChoice = userChoice;
+      Random rand = new Random();
+      ComputerChoice = _computerChoices[rand.Next(_computerChoices.Length)];
     }
   }
 }
